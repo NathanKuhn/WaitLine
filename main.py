67861bea0f83015d30e21dc992610319832558ae
kuhnn <kuhnnat000@gmail.com>
2021-11-14 09:33:20 -0800

    create window

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello")
+import pygame
+
+def main():
+
+    screen = pygame.display.set_mode((500, 500))
+
+    done = False
+
+    while not done:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        screen.fill((255, 255, 255))
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
