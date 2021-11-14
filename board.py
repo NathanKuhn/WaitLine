fb6d5856eca44a311fab7d6d042c1b03207f3b81
Pau <user@MacBook-Pro-de-User.local>
2021-11-14 13:38:00 -0800

    collision chairs

--- board.py
+++ board.py
@@ -58,21 +58,25 @@ class Board(pygame.sprite.Group):
         if MAP[y][x] == 0:
             table = element.Table(x, y)
             self.elements[y][x] = table
-            print("what up")
             self.add(table)
             if MAP[y-1][x] == 0:
                 chair = element.Chair(x,y-1)
-                self.elements[y][x] = chair
+                self.elements[y-1][x] = chair
                 self.add(chair)
             if MAP[y+1][x] == 0:
                 chair = element.Chair(x,y+1,180)
-                self.elements[y][x] = chair
+                self.elements[y+1][x] = chair
                 self.add(chair)
             if MAP[y][x+1] == 0:
                 chair = element.Chair(x+1,y,270)
-                self.elements[y][x] = chair
+                self.elements[y][x+1] = chair
                 self.add(chair)
             if MAP[y][x-1] == 0:
                 chair = element.Chair(x-1,y,90)
-                self.elements[y][x] = chair
+                self.elements[y][x-1] = chair
                 self.add(chair)
+
+    def checkCollision(self, x, y):
+       if (x < 0 or x >= len(MAP) or y < 0 or y >= len(MAP[0])):
+              return True
+       return (MAP[y][x] != 0 or self.elements[y][x] != 0)
